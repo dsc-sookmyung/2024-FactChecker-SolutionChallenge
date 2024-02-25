d3c0294af68ca94b0e68416050c110f1703859d7
CHAEYEON PARK <61193581+Yeon-chae@users.noreply.github.com>
2024-02-26 04:34:55 +0900

    Delete Server directory

--- Server/src/main/java/com/solutionchallenge/factchecker/api/Interests/dto/request/InterestRequestDto.java
+++
@@ -1,5 +0,0 @@
-package com.solutionchallenge.factchecker.api.Interests.dto.request;
-
-public class InterestRequestDto {
-    private String interestKeyword;
-}
--- Server/src/main/java/com/solutionchallenge/factchecker/api/Member/dto/request/MailConfirmRequest.java
+++
@@ -1,7 +0,0 @@
-package com.solutionchallenge.factchecker.api.Member.dto.request;
-import lombok.Data;
-@Data
-public class MailConfirmRequest {
-
-    public String email;
-}
